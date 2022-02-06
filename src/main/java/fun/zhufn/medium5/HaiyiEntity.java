fb78a0fcacbd6772ab96d9c6b91786cb64fbe12a
zhufengning <z@zhufn.fun>
2022-02-06 23:41:28 +0800

    给她一个牛的AI

--- src/main/java/fun/zhufn/medium5/HaiyiEntity.java
+++ src/main/java/fun/zhufn/medium5/HaiyiEntity.java
@@ -1,11 +1,14 @@
 package fun.zhufn.medium5;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.world.World;
 
-public class HaiyiEntity extends PathAwareEntity{
-    public HaiyiEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+public class HaiyiEntity extends CowEntity {
+    public HaiyiEntity(EntityType<? extends HaiyiEntity> entityType, World world) {
         super(entityType, world);
     }
 }
